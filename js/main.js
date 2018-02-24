961da06839950d7bf09cf2d732d829c1856935f0
NatBAt <natta7187@yandex.ru>
2018-02-24 22:01:27 +0300

    add slider

--- js/main.js
+++ js/main.js
@@ -1,23 +1,29 @@
-var nav = document.querySelector(".general-nav");
+// var nav = document.querySelector(".general-nav");
 
-var menu = nav.querySelector(".general-nav__site-list");
-var menuButton = nav.querySelector(".general-nav__toggle");
+// var menu = nav.querySelector(".general-nav__site-list");
+// var menuButton = nav.querySelector(".general-nav__toggle");
 
-menu.classList.remove("general-nav__site-list--no-js");
-menu.classList.add("general-nav__site-list--closed");
+// menu.classList.remove("general-nav__site-list--no-js");
+// menu.classList.add("general-nav__site-list--closed");
 
-menuButton.addEventListener("click", function (evt) {
-  evt.preventDefault();
+// menuButton.addEventListener("click", function (evt) {
+//   evt.preventDefault();
 
-  if (!menu.classList.contains("general-nav__site-list--opened")) {
-    menu.classList.add("general-nav__site-list--opened");
-    menu.classList.remove("general-nav__site-list--closed");
-    menuButton.classList.add("general-nav__toggle--closed");
-    menuButton.classList.remove("general-nav__toggle--opened");
-  } else {
-    menu.classList.remove("general-nav__site-list--opened");
-    menu.classList.add("general-nav__site-list--closed");
-    menuButton.classList.remove("general-nav__toggle--closed");
-    menuButton.classList.add("general-nav__toggle--opened");
-  }
+//   if (!menu.classList.contains("general-nav__site-list--opened")) {
+//     menu.classList.add("general-nav__site-list--opened");
+//     menu.classList.remove("general-nav__site-list--closed");
+//     menuButton.classList.add("general-nav__toggle--closed");
+//     menuButton.classList.remove("general-nav__toggle--opened");
+//   } else {
+//     menu.classList.remove("general-nav__site-list--opened");
+//     menu.classList.add("general-nav__site-list--closed");
+//     menuButton.classList.remove("general-nav__toggle--closed");
+//     menuButton.classList.add("general-nav__toggle--opened");
+//   }
+// });
+
+$(document).ready(function(){
+  $('.bxslider').bxSlider({
+    auto: true;
+  });
 });
